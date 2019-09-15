2962314392e32200e505cf5381ff46f23fa79600
Leaaaaaa <1440184465@qq.com>
2019-09-15 16:29:56 +0800

    commit leetcode day1 again

--- LeetCode_day1/src/day1/两数之和.java
+++ LeetCode_day1/src/day1/两数之和.java
@@ -3,5 +3,58 @@ package day1;/* *
 @date 2019/9/9 
 */
 
-public class 两数之和 {
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+
+//solution_1
+class Solution_1{
+    public int[] twoSum(int [] nums, int target){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] == target - nums[j]){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 }
+
+
+//solution_2
+class Solution_2{
+    public int[] twoSum(int [] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) != i){
+                return new int [] {i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+}
+
+//solution3
+class Solution_3{
+    public int[] towSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int [] {i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
