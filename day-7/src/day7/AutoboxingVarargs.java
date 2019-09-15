6a5311eb3b8fc997e70a81d9f7a50e82669d6343
Leaaaaaa <1440184465@qq.com>
2019-09-15 15:44:13 +0800

    commit java day7

---
+++ day-7/src/day7/AutoboxingVarargs.java
@@ -0,0 +1,7 @@
+package day7;/* *
+@author Lea
+@date 2019/9/14 
+*/
+
+public class AutoboxingVarargs {
+}
---
+++ day-7/src/day7/Chess.java
@@ -0,0 +1,7 @@
+package day7;/* *
+@author Lea
+@date 2019/9/15 
+*/
+
+public class Chess {
+}
