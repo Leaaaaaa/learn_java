157e677539710b9cadaf6982af7ea9d1842d294b
Leaaaaaa <1440184465@qq.com>
2019-09-15 16:08:50 +0800

    try commit java day5 again

--- day_5/src/day5/Exercise5_10.java
+++ day_5/src/day5/Exercise5_10.java
@@ -3,5 +3,28 @@ package day5;/* *
 @date 2019/9/10 
 */
 
-public class Exercise5_10 {
+class Finalize{
+    boolean flag = false;
+
+    void flagTrue() {
+        flag = true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable { //throws是用在方法声明后面，表示再抛出异常，由该方法的调用者来处理。
+        super.finalize();
+        if (!flag) {//如果flag为true则报错
+            System.err.println("flag is not true");
+
+        }
+    }
+}
+
+public class  Exercise5_10{
+    public static void main(String[] args) {
+        Finalize test = new Finalize();
+        test.flagTrue();
+        new Finalize();
+        System.gc();
+    }
 }
